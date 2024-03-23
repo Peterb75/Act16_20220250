13b5fdefb8398873424ec0faa051adc0419dce90
Peterb75 <a20220250@utem.edu.mx>
2024-03-22 20:22:39 -0600

    act16

---
+++ next-gen-ui/step_01/lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';   // Add this import
+import 'package:window_size/window_size.dart';
+
+import 'title_screen/title_screen.dart';
+
+void main() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowMinSize(const Size(800, 500));
+  }
+  Animate.restartOnHotReload = true;                     // Add this line
+  runApp(const NextGenApp());
+}
+
+class NextGenApp extends StatelessWidget {
+  const NextGenApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      home: const TitleScreen(),
+    );
+  }
+}
